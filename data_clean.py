588696c8e86e05165dd666058185cb48e469aefb
Jared Sage <jnsage@gmail.com>
2022-07-12 14:30:03 -0400

    ake .py scripts from notebook formats

---
+++ data_clean.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+# List of columns to drop. Columns are not used in this project. 
+def drop_columns(df: pd.DataFrame) -> pd.DataFrame:
+    drop_list = [
+             'numberVariant',
+                'released',
+                'category',
+                'bricksetURL',
+                'reviewCount',
+                'packagingType',
+                'availability',
+                'instructionsCount',
+                'additionalImageCount',
+                'lastUpdated',
+                'image.thumbnailURL',
+                'image.imageURL',
+                'collections.ownedBy',
+                'collections.wantedBy',
+                'dimensions.height',
+                'dimensions.width',
+                'dimensions.depth',
+                'LEGOCom.US.retailPrice',
+                'LEGOCom.US.dateFirstAvailable',
+                'LEGOCom.US.dateLastAvailable',
+                'LEGOCom.UK.retailPrice',
+                'LEGOCom.UK.dateFirstAvailable',
+                'LEGOCom.UK.dateLastAvailable',
+                'LEGOCom.CA.retailPrice',
+                'LEGOCom.CA.dateFirstAvailable',
+                'LEGOCom.CA.dateLastAvailable',
+                'dimensions.weight',
+                'barcode.EAN',
+                'barcode.UPC',
+                'minifigs',
+                'LEGOCom.DE.retailPrice',
+                'LEGOCom.DE.dateFirstAvailable',
+                'LEGOCom.DE.dateLastAvailable'
+                ]
+    df.drop(columns=drop_list, inplace=True)
