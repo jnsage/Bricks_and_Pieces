1f96c688444a8f1a3faa56e208a37904e948fd05
Jared Sage <jnsage@gmail.com>
2022-07-23 16:35:02 -0400

    update readme and gitignore

---
+++ PythonScripts/keys.py
@@ -0,0 +1,20 @@
+# API Keys 
+from pathlib import Path
+
+# Function for generating API key out of a text file
+
+def get_file_contents(filename: str):
+    cwd = Path.cwd().absolute()
+    key_path = Path.joinpath(cwd,'Keys',filename)
+    try:
+        with open(key_path, 'r') as f:
+            return f.read().strip()
+    except FileNotFoundError:
+        print("'%s' file not found" % filename)
+
+# Key for Rebrickable API
+KEY_ONE = get_file_contents('rebrickable.txt')
+
+#Key for Brickset API
+KEY_TWO = get_file_contents('brickset.txt')
+
